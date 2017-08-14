59b501cdd4d616a7d444f3fca302cbf662c57730
Hannes Füruter <hannes.fueruter@gmail.com>
2017-08-14 14:39:46 +0200

    Unity Project

---
+++ TopDownSurvival/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMove))]
+public class PlayerInput : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+    PlayerMove moveController = null;
+
+    private void Start()
+    {
+        if(moveController == null)
+        {
+            moveController = GetComponent<PlayerMove>();
+        }
+    }
+
+    void Update () {
+        moveInput();
+    }
+
+    private void moveInput()
+    {
+        float _moveX = Input.GetAxisRaw("Horizontal");
+        float _moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _horMove = Vector3.right * _moveX;
+        Vector3 _verMove = Vector3.forward * _moveZ;
+
+        Vector3 _movement = (_horMove + _verMove).normalized * moveSpeed;
+
+        moveController.setMovement(_movement);
+    }
+}
---
+++ TopDownSurvival/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+
+    private Vector3 movement;
+    private Rigidbody rb;
+
+	void Start () {
+      rb = GetComponent<Rigidbody>();
+      movement = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    void PerformMovement()
+    {
+        if (movement != null)
+        {
+            rb.transform.Translate(movement);
+        }
+    }
+
+    public void setMovement(Vector3 _movement)
+    {
+        movement = _movement;
+    }
+}
